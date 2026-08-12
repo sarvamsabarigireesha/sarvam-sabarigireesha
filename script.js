53694557b1b1054a2affb0f8b9b23afc9a3da6ef
sarvamsabarigireesha <sarvamsabarigireesha@gmail.com>
2026-08-12 13:13:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const btn=document.querySelector('.menu-toggle');const nav=document.querySelector('.main-nav');btn?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('.main-nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
