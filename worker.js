6169654781cefbb2b7c2ab3a44833047f1596135
sarvamsabarigireesha <sarvamsabarigireesha@gmail.com>
2026-09-13 20:49:54 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -9,61 +9,43 @@ if (
     request.method === "HEAD"
   )
 ) {
-
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/</loc>
   </url>
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/about/</loc>
   </url>
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/calendar/</loc>
   </url>
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/prasadam/</loc>
   </url>
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/journey/</loc>
   </url>
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/gallery/</loc>
   </url>
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/community/</loc>
   </url>
-
   <url>
     <loc>https://www.sarvamsabarigireesha.com/contact/</loc>
   </url>
-
 </urlset>`;
 
   return new Response(
-    request.method === "HEAD"
-      ? null
-      : sitemap,
+    request.method === "HEAD" ? null : sitemap,
     {
       status: 200,
       headers: {
-        "Content-Type":
-          "application/xml; charset=UTF-8",
-
-        "Cache-Control":
-          "public, max-age=300, must-revalidate",
-
-        "Access-Control-Allow-Origin":
-          "*",
-      },
+        "Content-Type": "application/xml; charset=UTF-8",
+        "Cache-Control": "public, max-age=300, must-revalidate",
+        "Access-Control-Allow-Origin": "*"
+      }
     }
   );
-
 }
